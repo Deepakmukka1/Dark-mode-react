41dc8532883ca93b9461f3fd299e080ab953e5f6
Mukka Deepak <ramu92013@gmail.com>
2021-06-13 17:54:33 +0530

    🎉 Initial commit

---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MainPage = ({content}) => {
+    return (
+        <div className="main-content">
+            <h1>{content}</h1>
+        </div>
+    )
+}
+
+export default MainPage
